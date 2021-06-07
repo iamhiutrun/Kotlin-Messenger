794370dc9c3dd584dcf11dc54904e6c2770033de
HiuTrun <62102580+HiuTrun@users.noreply.github.com>
2021-06-07 23:28:57 +0700

    Implement Firebase

--- app/src/main/java/hiutrun/me/kotlinmessenger/fragment/RegisterFragment.kt
+++ app/src/main/java/hiutrun/me/kotlinmessenger/fragment/RegisterFragment.kt
@@ -1,17 +1,33 @@
 package hiutrun.me.kotlinmessenger.fragment
 
+import android.content.ContentResolver
+import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import hiutrun.me.kotlinmessenger.MainActivity
 import hiutrun.me.kotlinmessenger.R
 import hiutrun.me.kotlinmessenger.databinding.FragmentRegisterBinding
+import hiutrun.me.kotlinmessenger.model.User
 import hiutrun.me.kotlinmessenger.viewmodel.LoginViewModel
 
 class RegisterFragment : Fragment(R.layout.fragment_register) {
 
     lateinit var binding: FragmentRegisterBinding
     lateinit var loginViewModel: LoginViewModel
+    var selectedImage : Uri?=null
+
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()){ uri: Uri?->
+        selectedImage = uri
+        val bitmap = MediaStore.Images.Media.getBitmap(requireContext().contentResolver,uri)
+        val bitmapDrawable = BitmapDrawable(bitmap)
+        binding.btnSelectPicture.setBackgroundDrawable(bitmapDrawable)
+    }
 
     companion object {
         @JvmStatic
@@ -32,13 +48,20 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
         binding.btnRegister.setOnClickListener {
             val email = binding.edtEmail.text.trim().toString()
             val password = binding.edtPassword.text.trim().toString()
+            val user = User(null,email, password,null)
             if(email.isNotEmpty() && password.isNotEmpty())
             loginViewModel.createUserWithEmailAndPassword(email,password)
+            loginViewModel.uploadImageToFirebaseStorage(user, selectedImage!!)
         }
         binding.tvSignIn.setOnClickListener {
             remove()
         }
+
+        binding.btnSelectPicture.setOnClickListener {
+            getContent.launch("image/*")
+        }
     }
+
 
     private fun remove(){
         val fm = requireActivity().supportFragmentManager
--- app/src/main/java/hiutrun/me/kotlinmessenger/viewmodel/LoginViewModel.kt
+++ app/src/main/java/hiutrun/me/kotlinmessenger/viewmodel/LoginViewModel.kt
@@ -1,17 +1,23 @@
 package hiutrun.me.kotlinmessenger.viewmodel
 
 import android.app.Application
+import android.net.Uri
 import android.util.Log
 
 import android.widget.Toast
 import androidx.lifecycle.AndroidViewModel
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
+import hiutrun.me.kotlinmessenger.model.User
+import java.util.*
 
 class LoginViewModel(
     app:Application
     ) : AndroidViewModel(app) {
 
     var firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
+    var firebaseStorage : FirebaseStorage = FirebaseStorage.getInstance()
 
     fun createUserWithEmailAndPassword(email:String, password: String){
         firebaseAuth.createUserWithEmailAndPassword(email,password)
@@ -36,5 +42,33 @@ class LoginViewModel(
             .addOnFailureListener {
                 Toast.makeText(getApplication(),it.message,Toast.LENGTH_LONG).show()
             }
+    }
+
+    fun uploadImageToFirebaseStorage(user:User,uri: Uri){
+        val filename = UUID.randomUUID().toString()
+        val ref = firebaseStorage.getReference("/images/$filename")
+        ref.putFile(uri).addOnSuccessListener {
+            Log.d("ABC", "uploadImageToFirebaseStorage: ${it.metadata?.path}")
+             ref.downloadUrl.addOnSuccessListener {
+                 Log.d("TAG", "uploadImageToFirebaseStorage: $it")
+                 user.profileImageUrl = it.toString()
+//                 saveUserToFirebaseDatabase(user)
+                 val ref = FirebaseDatabase.getInstance().getReference("/users/")
+                 ref.setValue("Hello")
+             }
+        }.addOnFailureListener{
+            Log.e("TAG", "uploadImageToFirebaseStorage: ${it.message}", )
+        }
+    }
+
+    private fun saveUserToFirebaseDatabase(user: User) {
+        val uid = firebaseAuth.uid
+        user.uid = uid!!
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$uid")
+        ref.setValue(user).addOnFailureListener {
+            Log.e("ABC", "saveUserToFirebaseDatabase: ${it.message}" )
+        }.addOnSuccessListener {
+            Log.d("TAG", "saveUserToFirebaseDatabase: ")
+        }
     }
 }
